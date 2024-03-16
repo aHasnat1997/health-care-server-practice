01127da7c016e2b2f5312a70a66fd2947a29c786
aHasnat1997 <hasnat56703@gmail.com>
2024-03-16 15:30:49 +0600

    api routes add

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import { AllRoutes } from './routes';
 
 const app = express();
+
+// express parsers
+app.use(express.json());
 app.use(cors());
 
+/**
+ * all api routes
+ */
+app.use('/api/v1', AllRoutes);
+
+/**
+ * Home route
+ */
 app.get('/', (req: Request, res: Response) => {
   res.status(200).json({
     message: 'Hello World...'
